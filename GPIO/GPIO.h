b01c356a4723dcb77a8b1fc8ee92b54174d9e91a
Ruslan <manianov.rus@gmail.com>
2016-05-12 11:13:09 +0600

    должно быть всё норм

--- GPIO/GPIO.h
+++ GPIO/GPIO.h
@@ -12,17 +12,16 @@
 #include "stm32F10x.h" 
 
 //==============================================================================
+//                      Константы и макросы
+//==============================================================================
+
+    // использование внешних прерываний (закоментировать если не используются)
+    #define USE_EXTI
+
+//==============================================================================
 //                  Структуры и типы
 //==============================================================================
 
-    // тригер
-    typedef enum
-    {
-        eGpioTriger_RISE  = EXTI_Trigger_Rising,            // от земли к питанию
-        eGpioTriger_FALL  = EXTI_Trigger_Falling,           // от питания к земле
-        eGpioTriger_ALL   = EXTI_Trigger_Rising_Falling,    // оба варианта
-    } eGpioTriger;
-       
     // Структура пина
     typedef struct 
     {
@@ -34,15 +33,10 @@
         GPIOMode_TypeDef    mode;       // режим работы пина
         uint32_t            remap;      // ремап
     } sGpio;
-<<<<<<< HEAD
 
 #ifdef USE_EXTI
 
-<<<<<<< HEAD
     // тип тригер
-=======
-    // тригер
->>>>>>> origin/master
     typedef enum
     {
         eGpioTriger_RISE  = EXTI_Trigger_Rising,            // от земли к питанию
@@ -50,18 +44,16 @@
         eGpioTriger_ALL   = EXTI_Trigger_Rising_Falling,    // оба варианта
     } eGpioTriger;
        
-=======
-    
->>>>>>> parent of b5c1293... Добавлен флаг состояния прерывания (вкл\выкл)
     // структура внешнего прерывания
     typedef struct
-    {
-        sGpio*              gpio;               // пин
-        eGpioTriger         triger;             // тригер прерывания
-        void                (*function)(void*); // указатель на внешний обработчик
-        void                (*object);          // объект использующий прерывание
+    {    
+        uint8_t         state;              // состояние внешнего прерывания (0 - выкл, 1 - вкл)
+        void            (*function)(void*); // указатель на внешний обработчик
+        void            (*object);          // объект использующий прерывание
     } sExtiHandler;
 
+#endif //USE_EXTI
+   
 //==============================================================================
 //                      Прототипы функций
 //==============================================================================
@@ -86,22 +78,18 @@
     
     // чтение значения выхода пина
     uint8_t GPIOReadOut (sGpio* gpio);
+
+#ifdef USE_EXTI    
     
     // конфигурация внешнего прерывания
-    void GPIOExtiConfig (eGpioTriger triger, uint8_t priority, void (*fuction)(void*), void (*object), sGpio* gpio);
+    void GPIOExtiConfig (uint8_t priority, void (*fuction)(void*), void (*object), sGpio* gpio);
     
     // включение внешнего прерывания
-    void GPIOExtiDisable (sGpio* gpio);
+    void GPIOExtiEnable (eGpioTriger triger, sGpio* gpio);
     
     // выключение внешнего прерывания
-    void GPIOExtiEnable (sGpio* gpio);
+    void GPIOExtiDisable (sGpio* gpio);
+    
+#endif //USE_EXTI
     
 #endif //__GPIO_H
-
-
-
-
-
-
-
-
